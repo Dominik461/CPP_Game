259b29a4360c63ef7e790bf1a1480fc36f3c8122
Dominik461 <neptunherbst2000@gmail.com>
2023-12-04 17:56:47 +0100

    Adjusted ability Fireball

--- CPP_Game/src/Abilities/Fireball.cpp
+++ CPP_Game/src/Abilities/Fireball.cpp
@@ -1,11 +1,7 @@
 #include "Header/Fireball.h"
 
-Fireball::Fireball(int cooldown, int value, std::string name, bool selfTarget)
-	:Ability(cooldown, value, name, selfTarget)
-{}
-
 Fireball::Fireball()
-	:Ability()
+	:Ability(3, 1.5, "Fireball")
 {}
 
 std::string Fireball::UseAbility()
@@ -13,9 +9,9 @@ std::string Fireball::UseAbility()
 	std::string logMsg = "";
 	if (m_currentCd == 0)
 	{
-
-		logMsg = m_casterName + " casts " + m_name + "! " + m_pTarget->GetName() + " takes " + std::to_string(m_value) + " damage!";
-		m_pTarget->m_curHp -= m_value;
+		int dmg = m_pCaster->GetDamge() * m_mulitplier;
+		logMsg = m_pCaster->GetName() + " casts " + m_name + "! " + m_pTarget->GetName() + " takes " + std::to_string(dmg) + " damage!";
+		m_pTarget->m_curHp -= dmg;
 		
 		m_currentCd = m_cd;
 		m_thisTurn = true;
--- CPP_Game/src/Abilities/Header/Fireball.h
+++ CPP_Game/src/Abilities/Header/Fireball.h
@@ -5,6 +5,5 @@ class Fireball : public Ability
 {
 public:
 	Fireball();
-	Fireball(int cooldown, int value, std::string name, bool selfTarget);
 	std::string UseAbility() override;
 };
