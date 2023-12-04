f511278900b3764d7d037463d78556b867d52ecd
Dominik461 <neptunherbst2000@gmail.com>
2023-12-04 18:03:00 +0100

    Adjusted ability Meteorstrike

--- CPP_Game/src/Abilities/MeteorStrike.cpp
+++ CPP_Game/src/Abilities/MeteorStrike.cpp
@@ -1,11 +1,7 @@
 #include "Header/MeteorStrike.h"
 
-MeteorStrike::MeteorStrike(int cooldown, int value, std::string name, bool selfTarget)
-	:Ability(cooldown, value, name, selfTarget)
-{}
-
 MeteorStrike::MeteorStrike()
-	:Ability()
+	:Ability(4, 2, "Meteorstrike")
 {}
 
 std::string MeteorStrike::UseAbility()
@@ -14,8 +10,9 @@ std::string MeteorStrike::UseAbility()
 	if (m_currentCd == 0)
 	{
 
-		logMsg = m_casterName + " casts " + m_name + "! " + m_pTarget->GetName() + " takes " + std::to_string(m_value) + " damage!";
-		m_pTarget->m_curHp -= m_value;
+		int dmg = m_pCaster->GetDamge() * m_mulitplier;
+		logMsg = m_pCaster->GetName() + " casts " + m_name + "! " + m_pTarget->GetName() + " takes " + std::to_string(dmg) + " damage!";
+		m_pTarget->m_curHp -= dmg;
 
 		m_currentCd = m_cd;
 		m_thisTurn = true;
